e0083a404135c4a6a75ad7fea1ea9fcf604bcf02
Pawel Potuczko <pawelpo99@gmail.com>
2025-01-23 15:04:07 +0100

    Shelves service added to LibraryController and added ShelfDTO

--- src/main/java/Libraries/Check/controller/LibraryController.java
+++ src/main/java/Libraries/Check/controller/LibraryController.java
@@ -1,4 +1,16 @@
 package Libraries.Check.controller;
 
+import Libraries.Check.service.ShelfService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "library")
 public class LibraryController {
+    private ShelfService shelfService;
+
+    public LibraryController(ShelfService shelfService) {
+        this.shelfService = shelfService;
+    }
+
 }
--- src/main/java/Libraries/Check/model/ShelfModel.java
+++ src/main/java/Libraries/Check/model/ShelfModel.java
@@ -29,10 +29,6 @@ public class ShelfModel {
         this.books = books;
     }
 
-    public ShelfModel(Long id) {
-        this.id = id;
-    }
-
     public Long getId() {
         return id;
     }
