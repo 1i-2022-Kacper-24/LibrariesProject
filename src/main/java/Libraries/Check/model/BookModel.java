a3cb569599bfb6824f7ce5db6f250671aad20cb8
Pawel Potuczko <pawelpo99@gmail.com>
2025-01-23 14:52:23 +0100

    Shelf model added with basic Book model parts

--- src/main/java/Libraries/Check/model/BookModel.java
+++ src/main/java/Libraries/Check/model/BookModel.java
@@ -1,4 +1,35 @@
 package Libraries.Check.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class BookModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public BookModel() {
+    }
+
+    public BookModel(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "BookModel{" +
+                "id=" + id +
+                '}';
+    }
 }
--- src/main/java/Libraries/Check/model/ShelfModel.java
+++ src/main/java/Libraries/Check/model/ShelfModel.java
@@ -1,4 +1,68 @@
 package Libraries.Check.model;
 
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class ShelfModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long shelfNumber;
+    @OneToMany(orphanRemoval = false)
+    private List<BookModel> books = new ArrayList<>();
+
+    public ShelfModel() {
+    }
+
+    public ShelfModel(Long id, Long shelfNumber) {
+        this.id = id;
+        this.shelfNumber = shelfNumber;
+    }
+
+    public ShelfModel(Long id, Long shelfNumber, List<BookModel> books) {
+        this.id = id;
+        this.shelfNumber = shelfNumber;
+        this.books = books;
+    }
+
+    public ShelfModel(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getShelfNumber() {
+        return shelfNumber;
+    }
+
+    public void setShelfNumber(Long shelfNumber) {
+        this.shelfNumber = shelfNumber;
+    }
+
+    public List<BookModel> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<BookModel> books) {
+        this.books = books;
+    }
+
+    @Override
+    public String toString() {
+        return "ShelfModel{" +
+                "id=" + id +
+                ", shelfNumber=" + shelfNumber +
+                ", books=" + books +
+                '}';
+    }
 }
