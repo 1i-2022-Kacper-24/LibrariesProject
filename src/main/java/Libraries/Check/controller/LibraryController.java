b74567c53571fdcc513ca72b2e12ea2f7bb9b767
1i-2022-Kacper-24 <1i.2022.kacper.24@gmail.com>
2025-01-23 12:40:07 +0100

    Demo code of controller for books

--- src/main/java/Libraries/Check/controller/LibraryController.java
+++ src/main/java/Libraries/Check/controller/LibraryController.java
@@ -1,4 +1,31 @@
 package Libraries.Check.controller;
 
+import Libraries.Check.model.BookModel;
+import Libraries.Check.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/Libraries")
 public class LibraryController {
+
+    private final BookService bookService;
+
+    @Autowired
+    public LibraryController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping("/ByAuthor")
+    public List<BookModel> findByAuthor(@RequestParam("author") String author) {
+        return bookService.findBooksByAuthor(author);
+    }
+
+    @PostMapping
+    public BookModel postBook(@RequestBody BookModel book) {
+        return bookService.addBook(book);
+    }
 }
