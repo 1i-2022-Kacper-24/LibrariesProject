c9d01db43cfdaba9e9d03624648acfb3d1ff7fe4
Pawel Potuczko <pawelpo99@gmail.com>
2025-01-23 14:54:55 +0100

    Shelf JpaRepository Implemented

--- src/main/java/Libraries/Check/repository/ShelfRepository.java
+++ src/main/java/Libraries/Check/repository/ShelfRepository.java
@@ -1,4 +1,11 @@
 package Libraries.Check.repository;
 
-public interface ShelfRepository {
+import Libraries.Check.model.BookModel;
+import Libraries.Check.model.ShelfModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ShelfRepository extends JpaRepository<ShelfModel, Long> {
+    ShelfModel findByBooksContains(BookModel book);
 }
