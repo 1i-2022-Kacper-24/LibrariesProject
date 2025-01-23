bfb3da4ffaacbc3a816167f05aa7b4c0f7363504
1i-2022-Kacper-24 <1i.2022.kacper.24@gmail.com>
2025-01-23 11:23:54 +0100

    added BookRepository and BookService demo code

--- src/main/java/Libraries/Check/service/BookService.java
+++ src/main/java/Libraries/Check/service/BookService.java
@@ -1,4 +1,37 @@
 package Libraries.Check.service;
 
+import Libraries.Check.model.BookModel;
+import Libraries.Check.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class BookService {
+
+    private final BookRepository bookRepository;
+
+    @Autowired
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public List<BookModel> findBooksByAuthor(String author) {
+        return bookRepository.findByAuthor(author);
+    }
+
+    public List<BookModel> findBooksByTitle(String title) {
+        return bookRepository.findByTitle(title);
+    }
+
+    public List<BookModel> findBooksByPubYear(int pubYear) {
+        return bookRepository.findByPubYear(pubYear);
+    }
+
+    public List<BookModel> findBooksByPages(int pages) {
+        return bookRepository.findByPages(pages);
+    }
+
+
 }
