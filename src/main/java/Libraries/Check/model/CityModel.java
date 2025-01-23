a76f69b8a12b5c2d44e61af682a19ab205a00a54
Pawel Potuczko <pawel.potuczko@sap.com>
2025-01-23 12:31:00 +0100

    Added City Model and Shelf Model for unidirectional relation

--- src/main/java/Libraries/Check/model/CityModel.java
+++ src/main/java/Libraries/Check/model/CityModel.java
@@ -1,4 +1,62 @@
 package Libraries.Check.model;
 
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class CityModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(orphanRemoval = true)
+    private List<ShelfModel> shelves = new ArrayList<>();
+
+    public CityModel() {
+    }
+
+    public CityModel(Long id, String name, List<ShelfModel> shelves) {
+        this.id = id;
+        this.name = name;
+        this.shelves = shelves;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<ShelfModel> getShelves() {
+        return shelves;
+    }
+
+    public void setShelves(List<ShelfModel> shelves) {
+        this.shelves = shelves;
+    }
+
+    @Override
+    public String toString() {
+        return "CityModel{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", shelves=" + shelves +
+                '}';
+    }
+
 }
--- src/main/java/Libraries/Check/model/ShelfModel.java
+++ src/main/java/Libraries/Check/model/ShelfModel.java
@@ -1,4 +1,36 @@
 package Libraries.Check.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class ShelfModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public ShelfModel() {
+    }
+
+    public ShelfModel(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "ShelfModel{" +
+                "id=" + id +
+                '}';
+    }
 }
