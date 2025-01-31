ee40f4e30c88d0c7951d2a3030bf863c5271ffbe
Pawel Potuczko <pawelpo99@gmail.com>
2025-01-31 14:39:48 +0100

    Moved all logic of check fields to query and built query to search by all fields

--- src/main/java/libraries/check/repository/BookRepository.java
+++ src/main/java/libraries/check/repository/BookRepository.java
@@ -11,27 +11,38 @@ import java.util.List;
 
 @Repository
 public interface BookRepository extends JpaRepository<BookModel, Integer> {
-    @Query(value = "SELECT b FROM BookModel b WHERE LOWER(b.author) LIKE LOWER(CONCAT('%', :author, '%'))")
-    List<BookModel> findByAuthor(@Param("author")String author);
+    /*  Search by any of parameters:
+        author - not null - text before passing should be checked for null
+        title - not null - text before passing should be checked for null
+        publicationYear - number
+        publicationIndicator - number that is setting how we search by publicationYear:
+                                (publicationIndicator > 0) greater than publicationYear
+                                (publicationIndicator = 0) equal to publicationYear
+                                (publicationIndicator < 0) less than publicationYear
+        numberOfPages - number
+        pagesIndicator - number that is setting how we search by numberOfPages:
+                                (pagesIndicator > 0) greater than numberOfPages
+                                (pagesIndicator = 0) equal to numberOfPages
+                                (pagesIndicator < 0) less than numberOfPages
+    */
+    @Query(value = "SELECT b FROM BookModel b " +
+            "WHERE LOWER(b.author) LIKE LOWER(CONCAT('%', :author, '%')) " +
+                "AND LOWER(b.title) LIKE LOWER(CONCAT('%', :title, '%')) " +
+                "AND (:publicationYear IS NULL OR :publicationIndicator IS NULL " +
+                    "OR (:publicationIndicator > 0 AND b.pubYear > :publicationYear) " +
+                    "OR (:publicationIndicator = 0 AND b.pubYear = :publicationYear) " +
+                    "OR (:publicationIndicator < 0 AND b.pubYear < :publicationYear)) " +
+                "AND (:numberOfPages IS NULL OR :pagesIndicator IS NULL " +
+                    "OR (:pagesIndicator > 0 AND b.pages > :numberOfPages) " +
+                    "OR (:pagesIndicator = 0 AND b.pages = :numberOfPages) " +
+                    "OR (:pagesIndicator < 0 AND b.pages < :numberOfPages))"
+    )
+    List<BookModel> findAllByParameters(@Param("author")String author,
+                                        @Param("title")String title,
+                                        @Param("publicationYear") Integer publicationYear,
+                                        @Param("publicationIndicator") Integer publicationIndicator,
+                                        @Param("numberOfPages") Integer numberOfPages,
+                                        @Param("pagesIndicator") Integer pagesIndicator
+                                        );
 
-    @Query(value = "SELECT b FROM BookModel b WHERE LOWER(b.title) LIKE LOWER(CONCAT('%', :title, '%'))")
-    List<BookModel> findByTitle(@Param("title")String title);
-
-    @Query(value = "SELECT b FROM BookModel b WHERE b.pubYear = :publicationYear")
-    List<BookModel> findByPubYear(@Param("publicationYear") int publicationYear);
-
-    @Query(value = "SELECT b FROM BookModel b WHERE b.pubYear > :publicationYear")
-    List<BookModel> findByPubYearGreaterThan(@Param("publicationYear") int publicationYear);
-
-    @Query(value = "SELECT b FROM BookModel b WHERE b.pubYear < :publicationYear")
-    List<BookModel> findByPubYearLessThan(@Param("publicationYear") int publicationYear);
-
-    @Query(value = "SELECT b FROM BookModel b WHERE b.pages = :numberOfPages")
-    List<BookModel> findByPages(@Param("numberOfPages") int numberOfPages);
-
-    @Query(value = "SELECT b FROM BookModel b WHERE b.pages > :numberOfPages")
-    List<BookModel> findByPagesGreaterThan(@Param("numberOfPages") int numberOfPages);
-
-    @Query(value = "SELECT b FROM BookModel b WHERE b.pages < :numberOfPages")
-    List<BookModel> findByPagesLessThan(@Param("numberOfPages") int numberOfPages);
 }
