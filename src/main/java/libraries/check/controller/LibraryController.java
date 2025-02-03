126db7c5ae4bf6039cd807c28dffdbabd889ed2f
Pawel Potuczko <pawelpo99@gmail.com>
2025-02-03 14:26:50 +0100

    changed endpoint to return list of libraryDTO

--- src/main/java/libraries/check/controller/LibraryController.java
+++ src/main/java/libraries/check/controller/LibraryController.java
@@ -3,6 +3,7 @@ package libraries.check.controller;
   
 import libraries.check.dto.BookFoundDTO;
 import libraries.check.dto.LibrariesDTO;
+import libraries.check.dto.LibraryDTO;
 import libraries.check.model.LibraryModel;
 import libraries.check.service.LibraryService;
 import libraries.check.service.ShelfService;
@@ -29,14 +30,10 @@ public class LibraryController {
         this.shelfService = shelfService;
         this.bookService = bookService;
     }
-  
-    public LibrariesDTO getAllLibraries() {
-        return (LibrariesDTO) LibrariesDTO.fromModel(libraryService.getAllLibraries());
-    }
 
-    @GetMapping("/allLibs")
-    public LibrariesDTO getAllLibs() {
-        return LibrariesDTO.fromModel(libraryService.getAllLibraries());
+    @GetMapping("/allLibraries")
+    public List<LibraryDTO> getAllLibs() {
+        return libraryService.getAllLibraries();
     }
 
     @GetMapping("/all")
